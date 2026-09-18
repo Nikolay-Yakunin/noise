408a53af2a408bc9f8d10a1f0c3a9179b6f1232a
Nikolay-Yakunin <akuninn52@gmail.com>
2026-09-18 21:23:21 +0700

    Add usage to readme, add persistance and octaves as cmd arguments

--- README.md
+++ README.md
@@ -2,6 +2,17 @@
 
 This is perlin noise!!! !!! !!! [source](https://habr.com/ru/articles/142592/)
 
+# Usage 
+
+```sh
+go run [width] [height] [persistence] [octaves]
+```
+
+Example
+```sh
+go run 2560 1440 0.5 5
+```
+
 ## Result
 
 ![image](./noise.png)
--- main.go
+++ main.go
@@ -59,6 +59,8 @@ func PerlinNoise2D(x, y, persistence float64, octaves int) float64 {
 }
 
 func main() {
+	per := 0.5
+	oct := 5
 	width, height := 2560, 1440
 	if len(os.Args) > 1 {
 		if n, err := strconv.Atoi(os.Args[1]); err == nil && n > 0 {
@@ -70,11 +72,21 @@ func main() {
 			height = n
 		}
 	}
+	if len(os.Args) > 3 {
+		if n, err := strconv.ParseFloat(os.Args[3], 64); err == nil && n > 0 {
+			per = n
+		}
+	}
+	if len(os.Args) > 4 {
+		if n, err := strconv.Atoi(os.Args[4]); err == nil && n > 0 {
+			oct = n
+		}
+	}
 
 	img := image.NewGray(image.Rect(0, 0, width, height))
 	for y := 0; y < height; y++ {
 		for x := 0; x < width; x++ {
-			value := PerlinNoise2D(float64(x)/16, float64(y)/16, 0.5, 5)
+			value := PerlinNoise2D(float64(x)/16, float64(y)/16, per, oct)
 			gray := uint8(math.Max(0, math.Min(255, (value+1)/2*255)))
 			img.SetGray(x, y, color.Gray{Y: gray})
 		}
