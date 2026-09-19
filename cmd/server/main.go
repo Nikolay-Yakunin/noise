566ff0b6975dca7244bda53231bb394c69c847f0
Nikolay-Yakunin <akuninn52@gmail.com>
2026-09-19 20:10:35 +0700

    add async endpoint and move html to him

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -59,5 +59,43 @@ func main() {
 		png.Encode(w, img)
 	})
 
+	http.HandleFunc("/noiseAsync", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "image/png")
+		per := 0.5
+		oct := 5
+		width, height := 2560, 1440
+
+		q := r.URL.Query()
+
+		// TODO: Move this bloc to package. used in 2 place, here and in cli
+		if n, err := strconv.Atoi(q.Get("width")); err == nil && n > 0 {
+			width = n
+		}
+		if n, err := strconv.Atoi(q.Get("height")); err == nil && n > 0 {
+			height = n
+		}
+		if n, err := strconv.ParseFloat(q.Get("per"), 64); err == nil && n > 0 {
+			per = n
+		}
+		if n, err := strconv.Atoi(q.Get("oct")); err == nil && n > 0 {
+			oct = n
+		}
+
+		res := noise.AsyncFlatPerlinNoise2D(width, height, per, oct)
+		img := image.NewGray(image.Rect(0, 0, width, height))
+
+		for y := range height {
+			offset := y * width
+			noiseRow := res[offset : offset+width]
+			for x := range width {
+				value := noiseRow[x]
+				gray := uint8(math.Max(0, math.Min(255, (value+1)/2*255)))
+				img.SetGray(x, y, color.Gray{Y: gray})
+			}
+		}
+
+		png.Encode(w, img)
+	})
+
 	log.Fatal(http.ListenAndServe(":9090", nil))
 }
